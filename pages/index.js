20465d8d7e802b982e0a0b5c32acd6c0ba9d2c79
alvinjamal <alvinjamalazkya@gmail.com>
2023-02-21 17:21:20 +0700

    Finish Url

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ import { Button } from "react-bootstrap";
 
 export async function getServerSideProps(context) {
   const cookie = context.req.headers.cookie;
-  console.log(cookie);
   const res = await axios.get(`${process.env.URL_BASE}/recipe`, {
     withCredentials: true,
     headers: {
